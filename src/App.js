5d12354dcc61e5fd8f5252db191033f48ff3d486
Madhubala2004 <shekarmadhubala@gmail.com>
2024-10-08 04:47:15 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       <h1>Madhubala S</h1>
+        <hr/>
+        <p>Mobile No:8438059501 Email Id:shehermadhubala@gmail.com Addres:sirkali 609115</p>
+        <hr/>
+        <p><h3>summary</h3></p>
+        <h4>I am innovate college student.Through Hand on project,internship collabration and using in Github and java my Goals on learning and explore my skills and knowledge to make a meaningful impact  in the world </h4>
+        <hr/>
+        <p><h3>skills</h3></p>
+        <h4>*Github*Git*HTML*Team work and collaboration </h4>        <hr></hr>
+        <p><h3>Education</h3></p>
+        <h5>Bachelor of computer science and Engineering,At verti vinayaha college of Engineering & Technology ,Thottiam. </h5>
+        Year:2024
+        
+       <h3>Languages </h3>
+        <h5>English first langugages </h5>
+        </div>
+    
+  );
+}
+
+export default App;
